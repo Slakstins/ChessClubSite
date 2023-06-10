a8dd5d3530e73c505263bdaee12b0038fdbdf5f1
Seth Lakstins <seth.lakstins@avanade.com>
2023-06-10 12:53:01 -0400

    changed background image behavior on tour page

--- src/app/tour-file-reader.service.ts
+++ src/app/tour-file-reader.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import * as tourData from '../assets/tour-dates.json';
+import tourData from '../assets/tour-dates.json';
 import { Perf } from './nav/tour/perf/perf.interface';
+
+// const tourData = require('../assets/tour-dates.json').data;
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +13,8 @@ export class TourFileReaderService {
   constructor() { }
   getTourData(): Perf[] {
     var perfs: Perf[] = [];
+    console.log(tourData[0]);
+    // console.log(tourData.length);
     for (let i = 0; i < Object.keys(tourData).length; i++){
       let tour = tourData[i];
       var perf: Perf = {
